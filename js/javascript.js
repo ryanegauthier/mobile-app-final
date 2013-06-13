90a9b9bc8a48ed6bcae8465204e57bd85a12048d
ryanegauthier <ryan.eugene.gauthier@gmail.com>
2013-06-13 14:33:35 -0700

    upload variable for map

--- js/javascript.js
+++ js/javascript.js
@@ -126,6 +126,7 @@ function onDeviceReady() {
 //
 function onSuccess(position) {
     var element = document.getElementById('geolocation');
+    var mapElement = document.getElementById('directionMap');
     element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
         'Longitude: '          + position.coords.longitude             + '<br />' +
         'Altitude: '           + position.coords.altitude              + '<br />' +
