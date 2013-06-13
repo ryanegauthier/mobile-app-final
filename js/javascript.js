11458a447bf72580b260e35b96665e0f3c97d900
ryanegauthier <ryan.eugene.gauthier@gmail.com>
2013-06-13 14:48:08 -0700

    clean js code

--- js/javascript.js
+++ js/javascript.js
@@ -19,26 +19,6 @@ function displayCart(num_items) {
     emailStr.attr('href', 'mailto:ryan.eugene.gauthier@gmail.com?subject=Pizza%20order&body=' + email_str + 'Pizza');
 }
 $(document).ready(function () {
-    // Add a buy button to each of my products
-
-//    $('div[data-product]').each(function () {
-//        $(this).append('<button class="btnCart">Add Topping</button>');
-//    });
-
-//    $('.btnCart').on("click", function(event) {
-//        var pid = $(this).parent().parent().data("product");
-//        console.log(pid);
-//        if (cart[pid]) {
-////                cart[pid].qty += 1;
-//        } else {
-//            cart[pid] = new Object();
-//            cart[pid].qty = 1;
-//            cart[pid].pid = pid;
-//            cart[pid].name = $(this).parent().siblings("span").data("name");
-////                cart[pid].price = $(this).parent().siblings("span").next().data("price");
-//        }
-//        displayCart();
-//    });
 
     $('.toggleswitch').on("slidestop", function(event) {
         var pid = $(this).parent().data("product");
@@ -68,10 +48,8 @@ $(document).ready(function () {
                 'class': 'myu-new-list',
                 html: data
             }).appendTo('.cartDiv');
-            //$("#cartDiv").append(data);
         });
-        //console.log( "http://cis217-4.gauthierdevelopment.net/buy.php?" + buyUrl);
-        //$("#buy").parent().hide();
+
     });
 
     $('#saveOrder').on("click", function (event) {
@@ -95,23 +73,6 @@ $(document).ready(function () {
         }
         displayCart();
     });
-
-//navigator.geolocation.getCurrentPosition(function (pos) {
-//    var lat = pos.coords.latitude;
-//    var lng = pos.coords.longitude;
-//    console.log(lat + " : " + lng);
-//    $("#lat").text(lat);
-//    $("#lng").text(lng);
-//    // var url = '<a href="http://maps.google.com/maps?q=' + lat + ',' + lng + '">Your Location</a>';
-//    // saddr=???&daddr=????
-//
-//    var url = '<a href="http://maps.google.com/maps?daddr=2234+E+Sprague+Ave,+Spokane,+WA+99207&saddr=' + lat + ',' + lng + '">Your Location</a>';
-//    $("#url").html(url);
-//
-//    var map = '<iframe width="360" height="360" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?daddr=2234+E+Sprague+Ave,+Spokane,+WA+99207&amp;saddr=' + lat + ',' + lng + '&amp;ie=UTF8&amp;t=h&amp;geocode=FRw31wIdgTgA-Q%3BFfOq1wIduLEA-SGSF4-H7rV7wSl16BgaKBmeVDGSF4-H7rV7wQ&amp;ll=47.67371,-117.410545&amp;spn=0.020806,0.030813&amp;z=14&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?daddr=2234+E+Sprague+Ave,+Spokane,+WA+99207&amp;saddr=47.65878000000001,-117.426047&amp;ie=UTF8&amp;t=h&amp;geocode=FRw31wIdgTgA-Q%3BFfOq1wIduLEA-SGSF4-H7rV7wSl16BgaKBmeVDGSF4-H7rV7wQ&amp;ll=47.67371,-117.410545&amp;spn=0.020806,0.030813&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left">View Larger Map</a></small>';
-//    $("#directionMap").html(map);
-//});
-
 // Wait for Cordova to load
 //
 document.addEventListener("deviceready", onDeviceReady, false);
